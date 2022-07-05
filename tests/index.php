741f63fc8a4c0386bb08d2f33b9028747d854f92
BAHC <alexwans@gmail.com>
2022-07-05 18:44:45 +0300

    update

--- tests/index.php
+++ tests/index.php
@@ -5,15 +5,19 @@
 </style>
 <?php
 include_once 'autoloader.php';
+
 //const DEBUG = true;
 const DEBUG = false;
 
-echo '<h3>Put 1 '. BAHC\DS::norm('A/A->A') .'</h3>';
+$DS = new BAHC\DS\DS;
+
+
+echo '<h3>Put 1 '. $DS::norm('A/A->A') .'</h3>';
 $_assertion = 'FAIL';
 $_key = 'a/a->a';
 $_value = 'AA_A';
-BAHC\DS::put($_key, $_value);
-$_res = BAHC\DS::get($_key);
+$DS::put($_key, $_value);
+$_res = $DS::get($_key);
 if (@\assert('AA_A' == $_res)){
     $_assertion = 'OK';
 }
@@ -26,8 +30,8 @@ echo '<h3>Get A/B</h3>';
 $_assertion = 'FAIL';
 $_key = 'a/b';
 $_value = 123;
-BAHC\DS::put($_key, $_value);
-$_res = BAHC\DS::get($_key);
+$DS::put($_key, $_value);
+$_res = $DS::get($_key);
 if (@\assert($_value == $_res)){
     $_assertion = 'OK';
 }
@@ -68,20 +72,20 @@ $_values = [
         ],
         
 ];
-BAHC\DS::putMany($_values);
+$DS::putMany($_values);
 $_value1 = 'big';
 $_value2 = 'Bunny';
 $_string1 = 'a/c->rabbits->rabbit->1';
 $_string2 = 'a/c->cows->cow->Rabbit';
-$_res1 = BAHC\DS::get($_string1);
-$_res2 = BAHC\DS::get($_string2);
+$_res1 = $DS::get($_string1);
+$_res2 = $DS::get($_string2);
 if (@\assert($_value1 == $_res1) && 
-        @\assert($_value2 == BAHC\DS::get($_string2))){
+        @\assert($_value2 == $DS::get($_string2))){
     $_assertion = 'OK';
 }
 if (DEBUG) {
-    echo 'value1 = ', $_value1,'; '. BAHC\DS::norm($_string1) . ' = ', $_res1, '<br />';
-    echo 'value2 = ', $_value2,'; '. BAHC\DS::norm($_string2). ' = ', $_res2, '<br />';
+    echo 'value1 = ', $_value1,'; '. $DS::norm($_string1) . ' = ', $_res1, '<br />';
+    echo 'value2 = ', $_value2,'; '. $DS::norm($_string2). ' = ', $_res2, '<br />';
 }
 echo '<p class="color-', $_assertion ,'">many() ', $_assertion, '<br />';
 
@@ -90,10 +94,10 @@ echo '<p class="color-', $_assertion ,'">many() ', $_assertion, '<br />';
 echo '<h3>One Tag</h3>';
 $_assertion = 'FAIL';
 $_value = 'Bunny';
-$_tag = BAHC\DS::norm('a/c->cows->cow->Rabbit');
+$_tag = $DS::norm('a/c->cows->cow->Rabbit');
 $_tags = 'rabbit';
 
-$a_tags = BAHC\DS::tags($_tags);
+$a_tags = $DS::tags($_tags);
 
 if (@\assert($_value == $a_tags[ $_tag ])) {
     echo 'value = ', $_value,'; tag value = ', $a_tags[$_tag], '<br />';
@@ -109,7 +113,7 @@ echo '<p class="color-', $_assertion ,'">tags() ', $_assertion, '<br />';
 echo '<h3>Many Tags</h3>';
 
 echo '<p style="padding: 6px; background-color: #EEE;">', 
-    json_encode(BAHC\DS::toArray(BAHC\DS::tags(['Rabbit', 'Cow']))), '</p>';
+    json_encode($DS::toArray($DS::tags(['Rabbit', 'Cow']))), '</p>';
 
 /* --------------------------------------- */
 
@@ -117,10 +121,10 @@ echo '<h3>Increment A/D</h3>';
 $_assertion = 'FAIL';
 $_key = 'a/d';
 $_value = 10;
-BAHC\DS::put($_key, 1);
-BAHC\DS::increment($_key, $_value);
+$DS::put($_key, 1);
+$DS::increment($_key, $_value);
 
-$_res = BAHC\DS::get($_key);
+$_res = $DS::get($_key);
 
 if (@\assert(11 == $_res)){
     echo 'value = ', $_value,'; ', $_key, ' = ', $_res, '<br />';
@@ -137,10 +141,10 @@ echo '<h3>Decrement A/E</h3>';
 $_assertion = 'FAIL';
 $_key = 'a/e';
 $_value = 1000;
-BAHC\DS::put($_key, 0);
-BAHC\DS::decrement($_key, $_value); //key 'a/e' = -1000;
+$DS::put($_key, 0);
+$DS::decrement($_key, $_value); //key 'a/e' = -1000;
 
-$_res = BAHC\DS::get($_key);
+$_res = $DS::get($_key);
 
 if (@\assert(-1000 == $_res)){
     echo 'value = ', $_value,'; ', $_key,' = ', $_res, '<br />';
@@ -157,12 +161,12 @@ echo '<h3>Forget Key</h3>';
 $_assertion = 'FAIL';
 $_value = null;
 $_key = 'a/e';
-BAHC\DS::put($_key, 'abc');
-BAHC\DS::forget($_key);
-$_res = BAHC\DS::get('a/e');
+$DS::put($_key, 'abc');
+$DS::forget($_key);
+$_res = $DS::get('a/e');
 if (@\assert($_value === $_res)){
     echo 'value = ', $_value,'; ', $_key,' = ', $_res, '<br />';
-    var_dump(BAHC\DS::get('a/e'));
+    var_dump($DS::get('a/e'));
     $_assertion = 'OK';
 }
 if (DEBUG) {
@@ -176,9 +180,9 @@ echo '<h3>Forget Keys</h3>';
 
 $_assertion = 'FAIL';
 $_keys = ['a/e->1'=>'abc', 'a/e->2'=>'def', 'a/e->3'=>'ghi'];
-BAHC\DS::putMany($_keys);
-BAHC\DS::forget(['a/e->2', 'a/e->3', 'Santa']);
-$_res = BAHC\DS::tags(['a/e->2', 'a/e->3']);
+$DS::putMany($_keys);
+$DS::forget(['a/e->2', 'a/e->3', 'Santa']);
+$_res = $DS::tags(['a/e->2', 'a/e->3']);
 
 if (@\assert(empty($_res))){
     $_assertion = 'OK';
@@ -195,9 +199,9 @@ echo '<p class="color-', $_assertion ,'">forget($keys) ', $_assertion, '<br />';
 
 echo '<h3>Flush</h3>';
 $_assertion = 'FAIL';
-$_value = count(BAHC\DS::getAll());
-BAHC\DS::flush();
-$count_ds = count(BAHC\DS::getAll());
+$_value = count($DS::getAll());
+$DS::flush();
+$count_ds = count($DS::getAll());
 
 if (@\assert(0 == $count_ds)){
     echo 'value = ', $_value, '; ', 'DS count = ', $count_ds, '<br />'; 
@@ -211,8 +215,8 @@ echo '<p class="color-', $_assertion ,'">flush() ', $_assertion, '<br />';
 
 /* NEXT, CURRENT, PREVIOUS --------------- */
 
-BAHC\DS::flush();
-BAHC\DS::putMany([
+$DS::flush();
+$DS::putMany([
     'a/a' => 'AA',
     'a/b' => 'AB',
     'a/c' => 'AC',
@@ -221,8 +225,8 @@ BAHC\DS::putMany([
     'a/c->c' => 'AC_C',
 ]);
 
-$ds = BAHC\DS::getAll();
-$count_ds = count($ds);
+$_ds = $DS::getAll();
+$count_ds = count($_ds);
 
 /* NEXT ---------------------------------- */
 
@@ -232,18 +236,18 @@ $_value = 'AC_B';
 
 $_index = 'a/c->a';
 echo 'Set index: ', $_index, '<br />';
-BAHC\DS::setIndex($_index);
+$DS::setIndex($_index);
 
-$index_value = BAHC\DS::next();
+$index_value = $DS::next();
 
 if (@\assert($_value == $index_value)){
     $_assertion = 'OK';
     echo 'value = ', $_value, '; ', 
-    'index_value = ', BAHC\DS::getIndex(), '<br />';
+    'index_value = ', $DS::getIndex(), '<br />';
     
     $_res = [];
     for ($i=0; $i< $count_ds; $i++) {
-        $_res[] = BAHC\DS::next();
+        $_res[] = $DS::next();
     }
     echo '<p>', implode(', ', $_res), '<br />';
 }
@@ -252,16 +256,16 @@ if (DEBUG) {
 }
 echo '<p class="color-', $_assertion ,'">next() ', $_assertion, '<br />';
 
-BAHC\DS::reset();
+$DS::reset();
 
 /* --------------------------------------- */
 
 echo '<h3>Current</h3>';
 
 $_value = 'AA';
-$_current = BAHC\DS::current();
+$_current = $DS::current();
 echo '<p>', $_current;
-echo ' : ', BAHC\DS::getIndex();
+echo ' : ', $DS::getIndex();
 
 if (@\assert($_value === $_current)) {
     $_assertion = 'OK';
@@ -273,7 +277,7 @@ if (DEBUG) {
 
     $_res = [];
     for ($i=0; $i<3; $i++) {
-        $_res[] = BAHC\DS::next() .' : '. BAHC\DS::current();
+        $_res[] = $DS::next() .' : '. $DS::current();
     }
     echo '<p>', implode(', ', $_res);    
 }
@@ -284,16 +288,16 @@ echo '<h3>Previous</h3>';
 
 $_index = 'a/b';
 echo 'Set index: ', $_index, '<br />';
-BAHC\DS::setIndex($_index);
+$DS::setIndex($_index);
 
 if (DEBUG) {
-    echo '<p>', BAHC\DS::current();
-    echo ' : ', BAHC\DS::getIndex();
+    echo '<p>', $DS::current();
+    echo ' : ', $DS::getIndex();
 }
 
 $_res = [];
 for ($i=0; $i<3; $i++) {
-    $_res[] = BAHC\DS::prev();
+    $_res[] = $DS::prev();
 }
 $_value = 'AA, AC_C, AC_B';
 $_result = implode(', ', $_res);
@@ -309,42 +313,40 @@ if (DEBUG) {
 
 echo '<p class="color-', $_assertion ,'">prev() ', $_assertion, '<br />';
 /* --------------------------------------- */
-function toArray() {
-    echo '<h3>toArray</h3>';
-    BAHC\DS::flush();
-    $_value = 'ABC';
-    $_assertion = 'FAIL';
-    $_data = [
-        'a' => 'A',
-        'a->b' => 'AB',
-        'a->b->c' => 'ABC',
-        null => '0',
-    
-    ];
 
-    BAHC\DS::putMany($_data);
-    $_array = BAHC\DS::toArray();
-    $_result = $_array['a']['b']['c'];
-    if (@\assert($_value == $_result)) {
-        $_assertion = 'OK';
-    }
+echo '<h3>toArray</h3>';
+$DS::flush();
+$_set['value'] = 'ABC';
+$_set['assertion'] = 'FAIL';
+$_set['data'] = [
+    'a' => 'A',
+    'a->b' => 'AB',
+    'a->b->c' => 'ABC',
+    null => '0',
+];
 
-    if (DEBUG) {
-        echo '<p>value = ', $_value, ' : result = ', $_result;
-        echo '<p>JSON: ', json_encode($_array), '<br />';
-    }
-    echo '<p class="color-', $_assertion ,'">toArray() ', $_assertion, '<br />';
+$DS::putMany($_set['data']);
+$_set['to_array'] = $DS::toArray();
+$_set['result'] = $_set['to_array']['a']['b']['c'];
+if (@\assert($_set['value'] == $_set['result'])) {
+    $_set['assertion'] = 'OK';
 }
 
-toArray();
+if (DEBUG) {
+    echo '<p>value = ', $_set['value'], ' : result = ', $_set['result'];
+    echo '<p>JSON: ', json_encode($_set['to_array']), '<br />';
+}
+echo '<p class="color-', $_set['assertion'] ,'">toArray() ', $_set['assertion'], '<br />';
+unset($_set);
+
 /*---------------------------------------------*/
 
 echo '<h3>toJson</h3>';
-BAHC\DS::flush();
+$DS::flush();
 
 
 $_value = '{"a":{"b":{"c":true,"d":false}},"e":{"f":{"g":"h","i":"j"}}}';
-BAHC\DS::put('response', $_value);
+$DS::put('response', $_value);
 
 $_assertion = 'FAIL';
 
@@ -356,13 +358,13 @@ $_data = '{
 }';
 
 $_data = json_decode($_data, true);
-BAHC\DS::putMany($_data, 'json');
+$DS::putMany($_data, 'json');
 
-$_tgs = BAHC\DS::tags('json');
-$_ds = BAHC\DS::toArray($_tgs);
+$_tgs = $DS::tags('json');
+$_ds = $DS::toArray($_tgs);
 
 $_rs = $_ds['json'];
-$_result = BAHC\DS::toJson( $_rs );
+$_result = $DS::toJson( $_rs );
 
 if (@assert($_value === $_result)) {
     $_assertion = 'OK';
@@ -378,3 +380,18 @@ if (DEBUG) {
 echo '<p class="color-', $_assertion ,'">toJson() ', $_assertion, '<br />';
 
 /*---------------------------------------------*/
+
+echo '<h3>Sum and SumAll</h3>';
+
+$DS::flush();
+$DS::put('one->red', 1);
+$DS::put('two->blue', 2);
+$DS::put('three->green', 3);
+$DS::put('four->blue->green', 4);
+$DS::put('five-red', 5);
+
+echo '<p>one + two + three = ', $DS::sum(['one', 'two', 'three']);
+echo '<p>reds: ', $DS::sum('red');
+echo '<p>greens: ', $DS::sum('green');
+echo '<p>blues: ', $DS::sum('blue');
+echo '<p>all: ', $DS::sumAll();
